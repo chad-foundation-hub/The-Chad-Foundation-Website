1de2882abada97de35a89a0c73f882c59e62dc80
The Chad Admin <124466222+Taniatos@users.noreply.github.com>
2025-11-19 21:31:07 -0500

    Update ChadMissionSupport.js

--- frontend/src/components/Pages/7-DonatePage/2-ChadMissionSupport/ChadMissionSupport.js
+++ frontend/src/components/Pages/7-DonatePage/2-ChadMissionSupport/ChadMissionSupport.js
@@ -1,5 +1,7 @@
 import React from "react";
 import "./ChadMissionSupport.css";
+import DonateSection from "../1-DonateSection/DonateSection";
+
 import MissionImageOne from "../../../../images/Donate-Images/missionSupport-image-one.png";
 import MissionImageTwo from "../../../../images/Donate-Images/missionSupport-image-two.png";
 
@@ -18,97 +20,100 @@ const ChadMissionSupport = () => {
   return (
     <section className="ChadMissionSupport">
       <div className="main-container">
-        <h2 className="chadMissionSupport-header">
-          Help Support the Chad Mission
-        </h2>
+        <DonateSection />
+        <div className="otherWays-main">
+          <h2 className="otherWays-header">
+            Help Support the Chad Mission
+          </h2>
 
-        {/* Book */}
-        <div className="chadMissionSupport-box">
-          <div className="chadMissionSupport-box-left-side">
-            <img
-              src={MissionImageOne}
-              alt="book"
-              className="chadMissionSupport-image"
-            />
+          {/* Book */}
+          <div className="otherWays-box">
+            <div className="otherWays-box-left-side">
+              <img
+                src={MissionImageOne}
+                alt="book"
+                className="otherWays-image"
+              />
+            </div>
+
+            <div className="otherWays-box-right-side">
+              <h3 className="otherWays-h3">
+                CHAD: A Celebration of Life Beyond a Mother’s Memories
+              </h3>
+              <p className="otherWays-p">
+                Celebrating the lives of young athletes lost to Sudden Cardiac
+                Death and the journey of its survivors, this uplifting memoir
+                details the 25-year history of The Chad Foundation for Athletes
+                and Artists to safeguard hearts by providing 10,000
+                Echocardiogram and EKG screenings in 5 states, and Austria, and
+                Sweden. It shares heartfelt stories of other Families who have
+                lost children to SCD and presents the history and importance of
+                heart screenings—its pros and cons— through scientific studies,
+                as well as the lifesaving tools—AEDs and CPR. The book has the
+                endorsement of Adam Silver, Commissioner of the National
+                Basketball Association. From Sudden Cardiac Victims to Survivors
+                — heartbreak to heroism — learn how The Chad Foundation and
+                countless advocate organizations, cardiologists, and health
+                professionals worldwide have safeguarded the lives of youth
+                through early preventive screening so they will have a better
+                future. 100% of the royalties from the CHAD Book go directly to
+                The Chad Foundation for Athletes and Artists, a nonprofit,
+                charitable organization, with a 501 c 3 tax-exempt status to
+                benefit the youth it serves. “The Gift of Heart and Art”
+              </p>
+              <button
+                className="otherWays-btn"
+                onClick={handleFirstButtonClick}
+              >
+                Donate with Purchase
+              </button>
+            </div>
           </div>
 
-          <div className="chadMissionSupport-box-right-side">
-            <h3 className="chadMissionSupport-h3">
-              CHAD: A Celebration of Life Beyond a Mother’s Memories
-            </h3>
-            <p className="chadMissionSupport-p">
-              Celebrating the lives of young athletes lost to Sudden Cardiac
-              Death and the journey of its survivors, this uplifting memoir
-              details the 25-year history of The Chad Foundation for Athletes
-              and Artists to safeguard hearts by providing 10,000 Echocardiogram
-              and EKG screenings in 5 states, and Austria, and Sweden. It shares
-              heartfelt stories of other Families who have lost children to SCD
-              and presents the history and importance of heart screenings—its
-              pros and cons— through scientific studies, as well as the
-              lifesaving tools—AEDs and CPR. The book has the endorsement of
-              Adam Silver, Commissioner of the National Basketball Association.
-              From Sudden Cardiac Victims to Survivors — heartbreak to heroism —
-              learn how The Chad Foundation and countless advocate
-              organizations, cardiologists, and health professionals worldwide
-              have safeguarded the lives of youth through early preventive
-              screening so they will have a better future. 100% of the royalties
-              from the CHAD Book go directly to The Chad Foundation for Athletes
-              and Artists, a nonprofit, charitable organization, with a 501 c 3
-              tax-exempt status to benefit the youth it serves. “The Gift of
-              Heart and Art”
-            </p>
-            <button
-              className="chadMissionSupport-btn"
-              onClick={handleFirstButtonClick}
-            >
-              Donate with Purchase
-            </button>
-          </div>
-        </div>
+          {/* KeyChain */}
+          <div className="otherWays-box">
+            <div className="otherWays-box-left-side">
+              <img
+                src={MissionImageTwo}
+                alt="book"
+                className="otherWays-image"
+              />
+            </div>
 
-        {/* KeyChain */}
-        <div className="chadMissionSupport-box">
-          <div className="chadMissionSupport-box-left-side">
-            <img
-              src={MissionImageTwo}
-              alt="book"
-              className="chadMissionSupport-image"
-            />
-          </div>
-
-          <div className="chadMissionSupport-box-right-side">
-            <h3 className="chadMissionSupport-h3">
-              “Life is a Gift” Keychain Pendant - Chad Safe Driver Campaign
-            </h3>
-            <p className="chadMissionSupport-p">
-              Chad was a great guy who loved all people, cared about their
-              dreams. He never drank, smoked or did drugs. He always was the
-              ‘designated driver’ when he and his friends went out to the clubs
-              dancing, and you always had to wear a seatbelt in Chad’s car.
-              Please safeguard your life and others by joining the{" "}
-              <strong>“Chad Safe Driver Campaign - Life is A Gift.”</strong>{" "}
-              Order your{" "}
-              <strong> “Life is A Gift” Keychain Shield Pendant,” </strong>a
-              heavyweight, curved shield pendant with a pewter-like finish. Join
-              the <strong>“Chad Honour Roll of Safe Drivers”</strong> by taking
-              the{" "}
-              <strong>
-                “Life is a Gift pledge: Save Lives, I will not text/drink and
-                drive.”
-              </strong>{" "}
-              Join the “Honor Roll of Safe Drivers” from all 50 states, and
-              countries world- wide. <strong>Hang it in your car</strong>, your
-              keychain, use as a piece of jewelry or give a gift as a reminder,{" "}
-              <strong>“Life is a Gift!”</strong> The Keychain Pendant comes in a
-              navy velveteen embossed drawstring pouch; high-quality embossed
-              navy gift box is $5 additional.
-            </p>
-            <button
-              className="chadMissionSupport-btn"
-              onClick={handleSecondButtonClick}
-            >
-              Order “Life is a Gift” Keychain
-            </button>
+            <div className="otherWays-box-right-side">
+              <h3 className="otherWays-h3">
+                “Life is a Gift” Keychain Pendant - Chad Safe Driver Campaign
+              </h3>
+              <p className="otherWays-p">
+                Chad was a great guy who loved all people, cared about their
+                dreams. He never drank, smoked or did drugs. He always was the
+                ‘designated driver’ when he and his friends went out to the
+                clubs dancing, and you always had to wear a seatbelt in Chad’s
+                car. Please safeguard your life and others by joining the{" "}
+                <strong>“Chad Safe Driver Campaign - Life is A Gift.”</strong>{" "}
+                Order your{" "}
+                <strong> “Life is A Gift” Keychain Shield Pendant,” </strong>a
+                heavyweight, curved shield pendant with a pewter-like finish.
+                Join the <strong>“Chad Honour Roll of Safe Drivers”</strong> by
+                taking the{" "}
+                <strong>
+                  “Life is a Gift pledge: Save Lives, I will not text/drink and
+                  drive.”
+                </strong>{" "}
+                Join the “Honor Roll of Safe Drivers” from all 50 states, and
+                countries world- wide. <strong>Hang it in your car</strong>,
+                your keychain, use as a piece of jewelry or give a gift as a
+                reminder, <strong>“Life is a Gift!”</strong> The Keychain
+                Pendant comes in a navy velveteen embossed drawstring pouch;
+                high-quality embossed navy gift box is $5 additional.
+              </p>
+              <button
+                className="otherWays-btn"
+                onClick={handleSecondButtonClick}
+              >
+                Order “Life is a Gift” Keychain
+              </button>
+            </div>
           </div>
         </div>
       </div>
