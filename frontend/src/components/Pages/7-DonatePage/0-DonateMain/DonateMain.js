7e096604ae362e4e7189c2fa56310c9c7df23281
The Chad Admin <124466222+Taniatos@users.noreply.github.com>
2025-11-19 21:30:50 -0500

    Update DonateMain.js

--- frontend/src/components/Pages/7-DonatePage/0-DonateMain/DonateMain.js
+++ frontend/src/components/Pages/7-DonatePage/0-DonateMain/DonateMain.js
@@ -1,14 +1,12 @@
 import React from "react";
 import "./DonateMain.css";
 import DonateNavigation from "../DonateNavigation";
-import DonateSection from "../1-DonateSection/DonateSection";
 import ChadMissionSupport from "../2-ChadMissionSupport/ChadMissionSupport";
 
 const DonateMain = () => {
   return (
     <section className="DonateMain">
       <DonateNavigation currentPage="donate-main" />
-      <DonateSection />
       <ChadMissionSupport />
     </section>
   );
