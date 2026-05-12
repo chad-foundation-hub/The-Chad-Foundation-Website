f940dc47a66762685427198b9d76752951088cb1
Taniatos <124466222+Taniatos@users.noreply.github.com>
2026-05-12 14:04:01 -0400

    Update HomePage.js

--- frontend/src/components/Pages/1-Home-Page/0-Home-Page-Main/HomePage.js
+++ frontend/src/components/Pages/1-Home-Page/0-Home-Page-Main/HomePage.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useLocation } from "react-router-dom";
 import HomeScreen from "../1-HomeScreen/HomeScreen";
 import StoryMission from "../2-StoryMission/StoryMission";
 import WhatYouShouldKnow from "../3-WhatYouShouldKnow/WhatYouShouldKnow";
@@ -6,8 +7,21 @@ import HowWeHelp from "../4-HowWeHelp/HowWeHelp";
 import SurvivorsStories from "../5-SurvivorsStories/SurvivorsStories";
 import ListenExperts from "../6-ListenExperts/ListenExperts";
 import Support from "../7-Support/Support";
+import LatestHeartNews from "../8-LatestHeartNews/LatestHeartNews";
 
 function HomePage() {
+  const location = useLocation();
+
+  useEffect(() => {
+    if (location.hash === "#latest-heart-news") {
+      const timer = setTimeout(() => {
+        const el = document.getElementById("latest-heart-news");
+        if (el) el.scrollIntoView({ behavior: "smooth" });
+      }, 150);
+      return () => clearTimeout(timer);
+    }
+  }, [location.hash]);
+
   return (
     <div>
       <HomeScreen />
@@ -17,6 +31,7 @@ function HomePage() {
       <SurvivorsStories />
       <ListenExperts />
       <Support />
+      <LatestHeartNews />
     </div>
   );
 }
